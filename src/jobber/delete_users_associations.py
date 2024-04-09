b0ea66c2c7a529e47775af219344c5ebb6192599
kr1sta1l <dmitriy_2003s@mail.ru>
2024-04-09 16:55:18 +0300

    Added delete_users_associations jobber

--- src/jobber/delete_users_associations.py
+++ src/jobber/delete_users_associations.py
@@ -4,40 +4,46 @@ import logging
 import aio_pika
 from src.config import config
 from src.modules.dao import UserDao
-from src.controllers.session import get_user_repository
+from src.controllers.session import get_user_repository, get_rabbit_url
 
 
 async def start_jobber() -> None:
     logging.info("Jobber started")
-    connection = await aio_pika.connect_robust(
-        host=config.RB_CONNECTION_HOST,
-        port=config.RB_CONNECTION_PORT
-    )
+    url = get_rabbit_url(config.RB_USERNAME, config.RB_PASSWORD, config.RB_CONNECTION_HOST,
+                         config.RB_CONNECTION_PORT, config.RB_VIRTUAL_HOST)
+    connection = await aio_pika.connect_robust(url=url)
 
     queue_name = config.LISTEN_QUEUE
 
     async with connection:
-        # Creating channel
         channel = await connection.channel()
 
-        # Will take no more than 10 messages in advance
         await channel.set_qos(prefetch_count=10)
 
-        # Declaring queue
-        queue = await channel.declare_queue(queue_name, auto_delete=True)
+        queue = await channel.declare_queue(queue_name, auto_delete=False, durable=True)
 
         async with queue.iterator() as queue_iter:
             async for message in queue_iter:
                 async with message.process():
                     logging.warning(message.body)
-                    body_dict = json.loads(message.body.decode())
-                    user_id = body_dict['userid']
+                    try:
+                        body_dict = json.loads(message.body.decode())
+                    except json.JSONDecodeError:
+                        logging.error("Message is not a json")
+                        continue
+                    try:
+                        user_id = body_dict['userid']
+                    except KeyError:
+                        logging.error("User id was not found in message")
+                        continue
 
                     user_repository = get_user_repository()
                     user_dao: UserDao = await user_repository.get_one_by_whereclause(UserDao.user_id == user_id)
                     if user_dao is not None:
                         await user_repository.delete(user_dao)
                         logging.info(f"User with id {user_id} was deleted")
+                    else:
+                        logging.warning(f"User with id {user_id} was not found")
     logging.warning("Jobber finished")
 
 
