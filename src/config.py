fabb3f1058d9206678c0b2a3b752e7edf5dbb23a
kr1sta1l <dmitriy_2003s@mail.ru>
2024-04-10 14:57:25 +0300

    Added default values

--- src/config.py
+++ src/config.py
@@ -7,33 +7,31 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class PydanticConfig(BaseSettings):
-    model_config = SettingsConfigDict(env_file=Path(os.path.dirname(os.path.realpath(__file__))) / '.env2',
+    model_config = SettingsConfigDict(env_file=Path(os.path.dirname(os.path.realpath(__file__))) / '.env',
                                       env_file_encoding='utf-8')
 
-    AS_HOST: Optional[AnyStr] = Field(None, env="AS_HOST")
-    AS_PORT: Optional[int] = Field(None, env="AS_PORT")
-    AS_LOG_LEVEL: Optional[AnyStr] = Field(None, env="AS_LOG_LEVEL")
+    AS_HOST: Optional[AnyStr] = Field("0.0.0.0", env="AS_HOST")
+    AS_PORT: Optional[int] = Field("8000", env="AS_PORT")
+    AS_LOG_LEVEL: Optional[AnyStr] = Field("INFO", env="AS_LOG_LEVEL")
     AS_LOG_FILE: Optional[AnyStr] = Field(None, env="AS_LOG_FILE")
 
-    PE_DB_DIALECT: Optional[AnyStr] = Field(None, env="PE_DB_DIALECT")
-    PE_DB_NOT_ASYNC_DIALECT: Optional[AnyStr] = Field(None, env="PE_DB_NOT_ASYNC_DIALECT")
-    PE_DB_USERNAME: Optional[AnyStr] = Field(None, env="PE_DB_USERNAME")
-    PE_DB_PASSWORD: Optional[AnyStr] = Field(None, env="PE_DB_PASSWORD")
-    PE_DB_HOST: Optional[AnyStr] = Field(None, env="PE_DB_HOST")
-    PE_DB_PORT: Optional[int] = Field(None, env="PE_DB_PORT")
-    PE_DB_DATABASE: Optional[AnyStr] = Field(None, env="PE_DB_DATABASE")
-    PE_DB_ECHO: Optional[bool] = Field(None, env="PE_DB_ECHO")
-    USER_SERVICE_HOST: Optional[AnyStr] = Field(None, env="USER_SERVICE_HOST")
-    USER_SERVICE_PORT: Optional[int] = Field(None, env="USER_SERVICE_PORT")
+    PE_DB_DIALECT: Optional[AnyStr] = Field("postgresql+asyncpg", env="PE_DB_DIALECT")
+    PE_DB_NOT_ASYNC_DIALECT: Optional[AnyStr] = Field("postgresql", env="PE_DB_NOT_ASYNC_DIALECT")
+    PE_DB_USERNAME: Optional[AnyStr] = Field("postgres", env="PE_DB_USERNAME")
+    PE_DB_PASSWORD: Optional[AnyStr] = Field("postgres", env="PE_DB_PASSWORD")
+    PE_DB_HOST: Optional[AnyStr] = Field("localhost", env="PE_DB_HOST")
+    PE_DB_PORT: Optional[int] = Field(5432, env="PE_DB_PORT")
+    PE_DB_DATABASE: Optional[AnyStr] = Field("auditorium_service", env="PE_DB_DATABASE")
+    PE_DB_ECHO: Optional[bool] = Field(False, env="PE_DB_ECHO")
 
     RB_CONNECTION_HOST: Optional[AnyStr] = Field("localhost", env="RB_CONNECTION_HOST")
     RB_CONNECTION_PORT: Optional[int] = Field(5672, env="RB_CONNECTION_PORT")
-    LISTEN_QUEUE: Optional[AnyStr] = Field("profile-delete-aud-queue", env="LISTEN_QUEUE")
-    SEND_QUEUE: Optional[AnyStr] = Field("auditorium-update-queue", env="SEND_QUEUE")
-    EXCHANGE: Optional[AnyStr] = Field("", env="EXCHANGE")
     RB_USERNAME: Optional[AnyStr] = Field("guest", env="RB_USERNAME")
     RB_PASSWORD: Optional[AnyStr] = Field("guest", env="RB_PASSWORD")
     RB_VIRTUAL_HOST: Optional[AnyStr] = Field("/", env="RB_VIRTUAL_HOST")
+    LISTEN_QUEUE: Optional[AnyStr] = Field("profile-delete-aud-queue", env="LISTEN_QUEUE")
+    SEND_QUEUE: Optional[AnyStr] = Field("auditorium-update-queue", env="SEND_QUEUE")
+    EXCHANGE: Optional[AnyStr] = Field("", env="EXCHANGE")
 
     @staticmethod
     def logging_level_strint_to_int(level: str) -> int:
