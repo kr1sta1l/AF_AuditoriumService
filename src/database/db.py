649cf03dbff6be8fb1162e2a535ab25907f9853b
kr1sta1l <dmitriy_2003s@mail.ru>
2024-04-10 18:32:58 +0300

    Fixed env parameters

--- src/database/db.py
+++ src/database/db.py
@@ -6,24 +6,24 @@ from functools import lru_cache
 @lru_cache
 def get_db_url():
     return URL.create(
-        config.PE_DB_DIALECT,
-        config.PE_DB_USERNAME,
-        config.PE_DB_PASSWORD,
-        config.PE_DB_HOST,
-        config.PE_DB_PORT,
-        config.PE_DB_DATABASE
+        config.AUD_DB_DIALECT,
+        config.AUD_DB_USERNAME,
+        config.AUD_DB_PASSWORD,
+        config.AUD_DB_HOST,
+        config.AUD_DB_PORT,
+        config.AUD_DB_DATABASE
     )
 
 
 @lru_cache
 def get_not_async_db_url():
     return URL.create(
-        config.PE_DB_NOT_ASYNC_DIALECT,
-        config.PE_DB_USERNAME,
-        config.PE_DB_PASSWORD,
-        config.PE_DB_HOST,
-        config.PE_DB_PORT,
-        config.PE_DB_DATABASE
+        config.AUD_DB_NOT_ASYNC_DIALECT,
+        config.AUD_DB_USERNAME,
+        config.AUD_DB_PASSWORD,
+        config.AUD_DB_HOST,
+        config.AUD_DB_PORT,
+        config.AUD_DB_DATABASE
     )
 
 
@@ -32,5 +32,5 @@ def get_db_settings():
     return {
         "not_async_url": get_not_async_db_url(),
         "database_url": get_db_url(),
-        "echo": config.PE_DB_ECHO,
+        "echo": config.AUD_DB_ECHO,
     }
